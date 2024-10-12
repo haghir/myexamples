9da1350436c617146c3a10b68b62eff18645182f
Your Name <you@example.com>
2024-10-12 18:57:54 +0900

    Make the pointer category.

--- rust/primitives/pointer/arc/src/main.rs
+++ rust/primitives/pointer/arc/src/main.rs
@@ -1,26 +1,31 @@
 use std::sync::Arc;
 
-fn bar(p: Arc<String>) {
+fn bar(o: String, p: Arc<String>, q: Arc<Box<String>>) {
     println!("bar strong count: {}", Arc::strong_count(&p));
     println!("bar weak count: {}", Arc::weak_count(&p));
+    println!("{:p}, {:p}, {:p}", &o, p, q);
 }
 
-fn foo(p: Arc<String>) {
+fn foo(o: String, p: Arc<String>, q: Arc<Box<String>>) {
     println!("foo strong count: {}", Arc::strong_count(&p));
     println!("foo weak count: {}", Arc::weak_count(&p));
+    println!("{:p}, {:p}, {:p}", &o, p, q);
 
-    bar(p.clone());
+    bar(o, p.clone(), q.clone());
 
     println!("foo strong count: {}", Arc::strong_count(&p));
     println!("foo weak count: {}", Arc::weak_count(&p));
 }
 
 fn main() {
+    let o = "text value".to_string();
     let p = Arc::new(String::from("text value"));
+    let q = Arc::new(Box::new(String::from("text value")));
     println!("strong count: {}", Arc::strong_count(&p));
     println!("weak count: {}", Arc::weak_count(&p));
+    println!("{:p}, {:p}, {:p}", &o, p, q);
 
-    foo(p.clone());
+    foo(o, p.clone(), q.clone());
 
     println!("strong count: {}", Arc::strong_count(&p));
     println!("weak count: {}", Arc::weak_count(&p));
